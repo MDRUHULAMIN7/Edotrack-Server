213e81e6c8c2b1fe521371acda5e9cdc612c8eac
Ruhul <ruhulthisis@gmail.com>
2025-01-25 19:41:29 +0600

    my-college

--- index.js
+++ index.js
@@ -342,6 +342,25 @@ const reviewsCollection = datbase.collection('reviews')
         res.status(500).json({ message: "Failed to fetch reviews." });
       }
     });
+
+    // get user profile
+    app.get("/users/:email", async (req, res) => {
+      const { email } = req.params;
+    
+      try {
+       
+        const user = await usersCollection.findOne({ email });
+    
+        if (!user) {
+          return res.status(404).json({ message: "User not found." });
+        }
+    
+        res.status(200).json(user);
+      } catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).json({ message: "Failed to fetch user." });
+      }
+    });
     
 
 
